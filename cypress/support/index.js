488d4534d8753ec23e5c07417e8feb7057ae9fcb
Diogo Sant'Anna <diogosantanna08@gmail.com>
2022-10-10 20:47:58 -0300

    iniciando projeto :rocket:

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,7 +15,6 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
-import '@david-boydell/cypress-layout';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
